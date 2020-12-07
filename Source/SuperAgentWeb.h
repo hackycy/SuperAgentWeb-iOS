50c2da6b94b12fffeb99c3ef448cd9ee8ec024a6
hackycy <qa894178522@qq.com>
2020-12-07 10:29:31 +0800

    feat：add Header file

--- Source/SuperAgentWeb.h
+++ Source/SuperAgentWeb.h
@@ -1,1 +1,12 @@
-// define
+//
+//  SuperAgentWeb.h
+//  SuperAgentWeb
+//
+//  Created by yzy on 2020/12/7.
+//
+
+#ifndef SuperAgentWeb_h
+#define SuperAgentWeb_h
+
+
+#endif /* SuperAgentWeb_h */
