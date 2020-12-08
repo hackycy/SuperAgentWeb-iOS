8e07b830c224687d1ba70f8ac14acaad8e538a6e
hackycy <qa894178522@qq.com>
2020-12-08 17:35:23 +0800

    feat：add pch

---
+++ Source/SuperAgentWebConfig.h
@@ -0,0 +1,19 @@
+//
+//  SuperAgentWebConfig.h
+//  SuperAgentWeb
+//
+//  Created by yzy on 2020/12/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+#define AGENTWEB_NAME @"SuperAgentWeb"
+
+@interface SuperAgentWebConfig : NSObject
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Source/SuperAgentWebConfig.m
@@ -0,0 +1,13 @@
+//
+//  SuperAgentWebConfig.m
+//  SuperAgentWeb
+//
+//  Created by yzy on 2020/12/8.
+//
+
+#import "SuperAgentWebConfig.h"
+
+@implementation SuperAgentWebConfig
+
+
+@end
